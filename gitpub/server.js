9a3f2d28d956db9e5cf388dd9f88c10eee568ef5
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-06-01 16:02:12 -0400

    added food

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,17 +2,26 @@ const express = require("express");
 const app = require("liquid-express-views")(express());
 const port = 3000;
 const drinks = require("./models/drinks.js");
+const food = require("./models/food.js");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
 });
 
 app.get("/drinks", (req, res) => {
-  res.render("index", { drinks });
+  res.render("index", { drinks, food });
 });
 
 app.get("/drinks/:id", (req, res) => {
-  res.render("show", { drink: drinks[req.params.id] });
+  res.render("show", {
+    render: drinks[req.params.id],
+  });
+});
+
+app.get("/food/:id", (req, res) => {
+  res.render("show", {
+    render: food[req.params.id],
+  });
 });
 
 app.listen(port, () => {
