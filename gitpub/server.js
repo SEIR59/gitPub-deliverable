83f8e582eaaa4838077de628965eccd4329ae904
annakoanna <anna.231991k@gmail.com>
2022-06-01 12:00:22 -0700

    drinks shows on page

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = require("liquid-express-views")(express())
+const drinks = require('./models/drinks.js')
+app.get('/drinks', (req, res)=> {
+    res.send(drinks)
+})
+
 app.get('/', (req, res)=>{
     res.send('Welcome to the Gitpub App!')
 })
