36e5aa3405c406cca6e885b2442423011e96f13d
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-06-01 14:42:29 -0400

    set up the drinks database

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = require("liquid-express-views")(express())
+const drinks = require("./models/drinks")
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
 })
 
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
 
 
 app.listen('3000', () => {
