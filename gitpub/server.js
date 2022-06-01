56f33af82fbc51a03e4c10da1c2eb492dfa3c4a3
patrickbasham <PatrickTBasham@gmail.com>
2022-06-01 17:08:48 -0400

    drinks 'database' is set up and displaying on '/drinks' route

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,10 +2,18 @@ const express = require("express")
 const app = require("liquid-express-views")(express())
 const port = 3000
 
+const drinks = require("./models/drinks.js")
+
+// main route for server.
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
+
+// make sure the server is up and listening
 app.listen(port,() => {
     console.log(`Listening on port: ${port}!`)
 })
