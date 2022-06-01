64b1a3adc80003640bff8a5bf0529831f4778767
patrickbasham <PatrickTBasham@gmail.com>
2022-06-01 17:43:36 -0400

    updated code to capitalize drink names, created base show.liquid file

--- gitpub/server.js
+++ gitpub/server.js
@@ -3,6 +3,18 @@ const app = require("liquid-express-views")(express())
 const port = 3000
 
 const drinks = require("./models/drinks.js")
+// function to capitalize the first letter of each drink name. returns NEW array.
+const capArrNames = (arrayObjs) => {
+    // change array to capitalize the first letter of each name
+    let arrDrinks = arrayObjs
+    for (let i = 0; i < drinks.length; i++) {
+        let currentDrink = arrDrinks[i].name
+        arrDrinks[i].name = currentDrink.charAt(0).toUpperCase() + currentDrink.slice(1)
+    }
+    return arrDrinks
+}
+
+const arrCapDrinks = capArrNames(drinks)
 
 // main route for server.
 app.get("/", (req, res) => {
@@ -11,14 +23,13 @@ app.get("/", (req, res) => {
 
 // route will display all drinks and info
 app.get("/drinks", (req, res) => {
-    // res.send(drinks)
-    let arrDrinks = drinks
-    // change array to capitalize the first letter of each name
-    for (let i = 0; i < drinks.length; i++) {
-        let currentDrink = arrDrinks[i].name
-        arrDrinks[i].name = currentDrink.charAt(0).toUpperCase() + currentDrink.slice(1)
-    }
-    res.render("index",{allDrinks: arrDrinks})
+    res.render("index",{allDrinks: arrCapDrinks})
+})
+
+// 
+app.get("/drinks/:id", (req, res) => {
+    res.send(req.params.id)
+    // res.render("index",{drink: arrCapDrinks})
 })
 
 // make sure the server is up and listening
