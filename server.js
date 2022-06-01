6482002a0ccb01dad5035dafe35053925d12991b
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-01 15:31:26 -0400

    Display the drinks in an unordered list with the first letter capitalized

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require('express');
 const app = require('liquid-express-views')(express());
 const port = 3000;
 
-const drinks = require('./models/drinks')
-
-
+const drinks = require('./models/drinks');
 
 app.get('/', (req, res) => {
   res.send(`Welcome to the Gitpub App`);
 });
 
 app.get('/drinks', (req, res) => {
-    res.render('index');
+  const updatedDrinks = drinks.map((drink) => {
+    const result = drink.name.replace(
+      drink.name[0],
+      drink.name.charAt(0).toUpperCase()
+    );
+    drink.name = result;
+    return drink;
   });
-  
+  res.render('index', { drinks:updatedDrinks });
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
