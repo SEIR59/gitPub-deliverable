a857dd2db044909dde72375e0d28be8c87e30b10
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-06-01 14:27:25 -0400

    server is set up and working

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express')
-const app = express()
+const app = require("liquid-express-views")(express())
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!');
+})
+
+
+
+app.listen('3000', () => {
+    console.log('port 3000 is active')
+})
