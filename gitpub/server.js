8f2d5cc856061659fc49bef7dcbbc7f40a9bed41
peterxu35 <peter.px.xu@gmail.com>
2022-06-01 18:19:49 -0400

    Set up database

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = require('liquid-express-views')(express())
 const port = 3000
 
+
+const drinks = require('./models/drinks')
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App')
+})
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
 })
 
 app.listen(port, () => {
