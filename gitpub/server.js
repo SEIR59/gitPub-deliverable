d0273f16fc25cd26aadda31a51d8670e4ae26638
patwyzlic <patrickwyzlic@gmail.com>
2022-06-01 19:50:02 -0400

    Imported drinks array

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = require("liquid-express-views")(express())
+const drinks = require('./models/drinks')
 
 let port = 3000;
 
@@ -9,4 +10,8 @@ app.listen(port, (req, res) =>  {
 
 app.get('/', (req, res) =>  {
     res.send('Welcome to the Gitpub App!');
+});
+
+app.get('/drinks', (req, res) =>  {
+    res.send(drinks);
 });
