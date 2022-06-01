ef2ab21ad8c94ca3bdf887e5b45c6359ca6a2540
NSTaft <taftnathan13@gmail.com>
2022-06-01 18:00:21 -0500

    Drinks database setup.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = require('liquid-express-views')(express())
 const port = 3000
+const drinks = require('./models/drinks.js')
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the GitPub App')
 })
 
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+
+
 
 app.listen(port, () => {
     console.log("Pub has opened")
 })
+
