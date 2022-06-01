c1d96b319867f763878883da4070f066224638b7
IanDLeon <ian.reggie.deleon@gmail.com>
2022-06-01 16:45:06 -0400

    Declared a variable drinks, imported & created a route /drinks

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require ("express");
 const app = require("liquid-express-views")(express())
+const drinks = require("./models/drinks.js")
 
 app.get("/", (req, res) => {
   res.send('Welcome to the Gitpub App!')
+})
+
+app.get("/drinks", (req, res) => {
+  res.send(drinks)
 })
 
 app.listen(3000, () => {
