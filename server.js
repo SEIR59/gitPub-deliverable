d38c6fa58460bbebfc608e37ed6363f6bdfa0ea9
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-06-01 17:14:06 -0400

    drinks file connected

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = require("liquid-express-views")(express());
 const port = 3000;
-
+const drinks = require('./models/drinks.js')
 
 app.listen(3000,()=>{
     console.log("Welcome to the Gitpub App!")
