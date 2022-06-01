6b04cd4aadd54b5355c0ad3b25716663206ed6c5
Robert Romano <bromano1992@gmail.com>
2022-06-01 19:49:09 -0400

    drinks json route

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,12 +1,19 @@
+// Setup for express/liquid and drinks export file
 const express = require('express')
 const app = require("liquid-express-views")(express())
+const drinks = require("./models/drinks.js")
 
 app.listen(3000, () => {
     console.log('listening on port 3000!')
 })
 
+// Initial testing of server setup
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
 })
 
+// drinks route as json
+app.get('/drinks', (req, res) => {
+        res.send(drinks)
+})
 
