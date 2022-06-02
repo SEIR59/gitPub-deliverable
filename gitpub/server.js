bfc30cc76ba6a3366c84346b3fbf0fbda53bd545
mileslathan <mlathan.cn@gmail.com>
2022-06-01 22:37:20 -0400

    Setting up your show route completed.

--- gitpub/server.js
+++ gitpub/server.js
@@ -14,6 +14,10 @@ app.get('/drinks', (req, res) => {
     })
 })
 
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
 app.listen(port, (req, res) =>{
     console.log("Listening to port 3000 :)")
 })
