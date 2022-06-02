9b66909e7bf4aaefddf8e12411e5ae88e7613b18
“GissellValdez” <Gissellvaldez@gmail.com>
2022-06-01 23:29:10 -0400

    created a get route for drinks that displays data in browser.

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = require("liquid-express-views")(express())
+const drinks = require('./models/drinks.js')
 const port = 3000
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}!!`)
 })
 
-
-
 app.get('/', (req, res) => {
 	res.send('Welcome to the Gitpub App!')
+})
+
+app.get('/drinks/', (req, res) => {
+	res.send(drinks)
+	
 })
