b6a5b67b86ac1b6bb69edb4e7cf2de2938f0ef8c
Freightscape <jordan@freightscape.net>
2022-06-02 06:12:50 -0400

    Setting up your database - COMPLETE

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = require("liquid-express-views")(express());
 const port = 3000;
 
+const drinks = require("./models/drinks.js");
+
 app.listen(port, () => {
   console.log("This port is working");
 });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
+});
+
+app.get("/drinks", (req, res) => {
+  res.send(drinks);
 });
