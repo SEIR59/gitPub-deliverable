6b25664ff6c582c836156d0ee2e609a6b521305d
mileslathan <mlathan.cn@gmail.com>
2022-06-01 22:10:36 -0400

    Setting up your index view completed

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const app = express()
+const app = require("liquid-express-views")(express())
 const port = 3000
 const drinks = require('./models/drinks.js')
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the Gitpub App!")
+    res.send("Welcome to the GitPub App!")
 })
 
 app.get('/drinks', (req, res) => {
-    res.send(drinks)
+    res.render('index', 
+    {
+    allDrinks: drinks
+    })
 })
 
 app.listen(port, (req, res) =>{
