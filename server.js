9251ca31cef1b306aa2a4599aa561567666803e5
“GissellValdez” <Gissellvaldez@gmail.com>
2022-06-02 19:51:06 -0400

    setup show route.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = require('liquid-express-views')(express())
 const drinks = require('./models/drinks.js')
 const port = 3100
@@ -15,4 +16,8 @@ app.get('/drinks/', (req, res) => {
 	res.render('index.liquid', { 
 		allOfTheDrinks: drinks,
 	})
+})
+
+app.get('/drinks/:id', (req,res) => {
+	res.send(req.params.id)
 })
